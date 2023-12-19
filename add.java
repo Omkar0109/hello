1fac063b8a6aa40a5222c8c621a53404a6bd3532
Omkar0109 <sobp9999@gmail.com>
2023-12-19 09:48:35 +0530

    Change

--- add.java
+++ add.java
@@ -2,7 +2,7 @@ class add
 {
     public static void main(String[]args)
     {
-        int a=4;int b=5;
-        System.out.println("Add :"+" "+(a+b));
+        int a=4;int b=5;int c=6;
+        System.out.println("Add :"+" "+(a+b+c));
     }
 }
